0ef48a4c45837db45bc1b35bbf2c514867ee702d
sdlfkorg <sdlfkorg@gmail.com>
2018-07-10 17:33:50 +0800

    clean code

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -6,12 +6,11 @@ export default function(state = {}, action){
 	switch (action.type){
 
 		case FETCH_POSTS:
-			console.log(action.payload.data);
-			return _.mapKeys(action.payload.data, 'id')
-
+			return _.mapKeys(action.payload.data, 'id');
 
 		default:
 			return state;
+
 
 	}
 
