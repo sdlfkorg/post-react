5e6134885abff6f4c737e075dd70cbd80de5ba28
sdlfkorg <sdlfkorg@gmail.com>
2018-07-10 17:35:47 +0800

    components: fetch post list

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,13 +1,36 @@
+import _ from 'lodash';
 import React, { Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions';
+
 
 class PostIndex extends Component {
+	componentDidMount(){
+		this.props.fetchPosts();
+	}
+
+	renderPosts(){
+		_.map(this.props.posts, post => {
+			return (
+				<li className="list-group-item" key={post.id}>
+					{post.title}
+				</li>
+			);
+		});
+	}
+
 	render(){
+		console.log(this.props);
 		return (
 			<div>
-				PostIndex
+				Posts
 			</div>
 		);
 	}
 }
 
-export default PostIndex;
+function mapStateToProps(state){
+	return {posts: state.posts};
+}
+
+export default connect(mapStateToProps, {fetchPosts})(PostIndex);
