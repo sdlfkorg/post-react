9749287c6605e798fdc4caf3575bc74926d8f5d5
sdlfkorg <sdlfkorg@gmail.com>
2018-07-11 11:28:13 +0800

    fix stupid import bug

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 // do not need to specify the file, because import from index.js
-import FETCH_POSTS from '../actions';
+import {FETCH_POSTS} from '../actions';
 
 export default function(state = {}, action){
 	switch (action.type){
