aef361f920a5f9f5c809e5d9e4a4dafa4d0d2234
sdlfkorg <sdlfkorg@gmail.com>
2018-07-11 14:11:18 +0800

    components: add a link to /posts/new

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import React, { Component} from 'react';
 import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 import {fetchPosts} from '../actions';
 
 
@@ -23,6 +24,11 @@ class PostIndex extends Component {
 		console.log(this.props);
 		return (
 			<div>
+				<div className="text-xs-right">
+					<Link className="btn btn-primary" to="/posts/new">
+						Add a post
+					</Link>
+				</div>
 				<h3>Posts</h3>
 				<ul className="list-group">
 					{this.renderPosts()}
