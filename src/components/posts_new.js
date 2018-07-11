52830030cde447de5e456a17ab5855648c787147
sdlfkorg <sdlfkorg@gmail.com>
2018-07-11 16:35:36 +0800

    wire redux-from up for creating post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,33 @@
 import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
+
 
 class PostsNew extends Component{
+	renderTitleField(field){
+		return (
+			<div>
+				<input
+					type="text"
+					{...field.input}
+				/>
+			</div>
+		);
+	}
+
 	render(){
 		return (
 			<div>
-				PostNew!
+				<form>
+					<Field
+						name="title"
+						component={this.renderTitleField}
+					/>
+				</form>
 			</div>
 		);
 	}
 }
 
-export default PostsNew;
+export default reduxForm({
+	form: 'PostNewForm'
+})(PostsNew);
