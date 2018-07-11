f08fbdc870de9f6c05cfbb1d7584ea35affad08b
sdlfkorg <sdlfkorg@gmail.com>
2018-07-11 20:48:47 +0800

    components: add form for creat post and set up form validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,10 +3,12 @@ import {Field, reduxForm} from 'redux-form';
 
 
 class PostsNew extends Component{
-	renderTitleField(field){
+	renderField(field){
 		return (
-			<div>
+			<div className="form-group">
+				<label>{field.label}</label>
 				<input
+					className="form-control"
 					type="text"
 					{...field.input}
 				/>
@@ -19,15 +21,52 @@ class PostsNew extends Component{
 			<div>
 				<form>
 					<Field
+						label="Title"
 						name="title"
-						component={this.renderTitleField}
+						component={this.renderField}
 					/>
+					<Field
+						label="Categories"
+						name="categories"
+						component={this.renderField}
+					/>
+					<Field
+						label="Post Content"
+						name="content"
+						component={this.renderField}
+					/>
+					
 				</form>
 			</div>
 		);
 	}
 }
 
+function validate(values){
+	// console.log(values);
+	const errors = {};
+
+	// validate the inputs from 'values'
+	if (!values.title){
+		errors.title = 'Enter a title';
+	}
+
+	if (!values.categories){
+		errors.categories = 'Enter a category';
+	}
+
+	if (!values.content){
+		errors.content = 'Enter some content';
+	}
+
+
+	// if errors is empty, the form is fine to submit
+	// if errors has any property, redux-form assumes the form is invalid.
+	return errors;
+
+}
+
 export default reduxForm({
+	validate: validate,
 	form: 'PostNewForm'
 })(PostsNew);
