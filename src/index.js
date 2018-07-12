24b3937d45f706932fe577b233d744e21dae4642
sdlfkorg <sdlfkorg@gmail.com>
2018-07-12 16:56:33 +0800

    setup action and route for post_detail view

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import promise from 'redux-promise';
 import reducers from './reducers';
 import PostIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
+import PostDetail from './components/post_detail';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -19,6 +20,7 @@ ReactDOM.render(
 	    <div>
 	    	<Switch>
 		    	<Route path="/posts/new" component={PostsNew} />
+		    	<Route path="/posts/:id" component={PostDetail} />
 		    	<Route path="/" component={PostIndex} />
 	    	</Switch>
     	</div>
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,12 +1,18 @@
 import _ from 'lodash';
 // do not need to specify the file, because import from index.js
-import {FETCH_POSTS} from '../actions';
+import {FETCH_POSTS, FETCH_POST} from '../actions';
 
 export default function(state = {}, action){
 	switch (action.type){
 
 		case FETCH_POSTS:
 			return _.mapKeys(action.payload.data, 'id');
+		case FETCH_POST:
+			const post = action.payload.data;
+			const newState = {...state};
+			newState[post.id] = post;
+			return newState;
+			// or return {...state, [action.payload.data.id]: action.payload.data};
 
 		default:
 			return state;
